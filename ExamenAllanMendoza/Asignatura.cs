832c7a2a16a3db4afde6782d59aa5a4dd85e358c
Allitan <allanmendoza036@gmail.com>
2025-02-19 19:08:50 -0600

    Update Asignatura.cs
    todo de la clase asignatura, pense que se habia subido ya

--- ExamenAllanMendoza/Asignatura.cs
+++ ExamenAllanMendoza/Asignatura.cs
@@ -1,12 +1,61 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
 
 namespace ExamenAllanMendoza
 {
-    internal class Asignatura
+    public class Asignatura : Alumno , IAsignatura
     {
+        public int N1 { get; set; }
+        public int N2 { get; set; }
+        public int N3 { get; set; }
+        public string? NombreAsingnatura { get; set; }
+        public string? Horario { get; set; }
+        public string? NombreDocente { get; set; }
+
+        public double CalcularNotaFinal()
+        {
+            return N1 + N2 + N3;
+        }
+
+        public double CalcularNotaFinal(int n1, int n2, int n3)
+        {
+            return n1 + n2 + n3;
+        }
+
+        public string? MensajeNotaFinal(double notaFinal)
+        {
+            if (notaFinal >= 90 && notaFinal <= 100)
+            {
+                return "Sobresaliente";
+
+            }
+            else if (notaFinal >= 80 && notaFinal <= 89)
+            {
+
+                return "Muy Bueno";
+            }
+            else if (notaFinal >= 60 && notaFinal <= 79)
+            {
+                return "Bueno";
+            }
+            else
+            {
+                return "Reprobado";
+            }
+        }
+        public void Imprimir()
+        {
+            double notaFinal = CalcularNotaFinal();
+
+            Console.WriteLine("***********************************************************************************");
+            Console.WriteLine($"Nombre del estudiante: {NombreAlumno}");
+            Console.WriteLine($"Numero de cuenta: {NumeroCuenta}");
+            Console.WriteLine($"Correo electronico: {Email}");
+            Console.WriteLine($"Nombre de la clase: {NombreAsingnatura}");
+            Console.WriteLine($"Horario: {Horario}");
+            Console.WriteLine($"Nombre del docente: {NombreDocente}");
+            Console.WriteLine($"Nota Final: {notaFinal} - {MensajeNotaFinal(notaFinal)}");
+            Console.WriteLine($"Nota Final: {CalcularNotaFinal(N1,N2,N3)} - {MensajeNotaFinal(notaFinal)}");
+            Console.WriteLine("***********************************************************************************");
+        }
     }
 }
