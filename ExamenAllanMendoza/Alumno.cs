d92907ba85f5b2f2d50efbb3f84e726872c04b80
Allitan <allanmendoza036@gmail.com>
2025-02-19 18:22:38 -0600

    Aniadiendo la clase Alumno

---
+++ ExamenAllanMendoza/Alumno.cs
@@ -0,0 +1,12 @@
+﻿
+
+namespace ExamenAllanMendoza
+{
+    public class Alumno
+    {
+        public string? NombreAlumno {  get; set; }
+        public string? NumeroCuenta { get; set; }
+        public string? Email { get; set; }
+
+    }
+}
